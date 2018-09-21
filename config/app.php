f3b80d2cc7ef64e5335c920c717419423ce72022
boripat2518 <mr.boripat@gmail.com>
2018-09-21 16:18:26 +0700

    Composer and config
    - Composer "laravel/socialite"
    - update config/app.php

--- config/app.php
+++ config/app.php
@@ -161,6 +161,7 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         Laravel\Passport\PassportServiceProvider::class,
+        Laravel\Socialite\SocialiteServiceProvider:class,
 
     ],
 
@@ -210,6 +211,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Socialite' ==> Laravel\Socialite\Facades\Socialite::class,
 
     ],
 
